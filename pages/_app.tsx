6c6274e19886579efad4dc07871bb9b15159bad1
Doryski <dominatorskyyy@gmail.com>
2021-03-03 08:32:00 +0100

    added next-seo, temporarily same for all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import ThemeStyleProvider from '../theme/ThemeStyleProvider'
 import { Provider } from 'next-auth/client'
+import { NextSeo } from 'next-seo'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
 	<React.StrictMode>
+		<NextSeo
+			title='Job offers | IT sector'
+			description='A platform with job offers for IT sector.'
+		/>
 		<Provider session={pageProps.session}>
 			<ThemeStyleProvider>
 				<Component {...pageProps} />
