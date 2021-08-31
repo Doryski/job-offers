969c47a1e6a4d1ef2b2157f8c766e23162dcc56b
Doryski <dominatorskyyy@gmail.com>
2021-08-31 22:40:40 +0200

    structure: updated types for packages

--- hooks/usePrefetch.ts
+++ hooks/usePrefetch.ts
@@ -1,9 +1,23 @@
 import { NextRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
-export default function usePrefetch(url: string, router: NextRouter): void {
+export default async function usePrefetch(
+	url: string,
+	router: NextRouter
+): Promise<boolean> {
+	const [isFetched, setIsFetched] = useState(false)
+	const prefetch = useCallback(async () => {
+		await router.prefetch(url)
+		setIsFetched(true)
+	}, [router, url])
+
 	useEffect(() => {
-		router.prefetch(url)
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
+		prefetch()
+
+		return () => {
+			setIsFetched(false)
+		}
+	}, [prefetch, router, url])
+
+	return isFetched
 }
